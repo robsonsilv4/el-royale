060a165f346252e601262e70785d6add1f40af50
robsonsilv4 <robsonsilv410@gmail.com>
2019-09-17 17:04:45 -0300

    Adicionado criaçao do superuser

--- users/models.py
+++ users/models.py
@@ -16,6 +16,14 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, name, email, password):
+        user = self.create_user(name, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField('Nome', max_length=255)
